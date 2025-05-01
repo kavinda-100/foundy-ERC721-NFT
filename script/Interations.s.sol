d7b1d7a1e7e9133fee02bc05bae3c8244de09f36
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-01 12:57:42 +0530

    feat: implement MintBasicNFT script for NFT minting with IPFS token URI

--- script/Interations.s.sol
+++ script/Interations.s.sol
@@ -4,14 +4,21 @@ pragma solidity ^0.8.25;
 import {BasicNFT} from "../src/BasicNFT.sol";
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNFT is Script {
-    function run() external {}
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    function mintNFT(address basicNFT, string memory tokenUri) public {
-        (bool success, ) = basicNFT.call(
-            abi.encodeWithSignature("mintNFT(string)", tokenUri)
-        );
-        require(success, "Minting failed");
+    function run() external {
+        address mostRecentlyDeployBasicNFT = DevOpsTools
+            .get_most_recent_deployment("BasicNFT", block.chainid);
+        mintNFTOnContract(mostRecentlyDeployBasicNFT);
+    }
+
+    function mintNFTOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(contractAddress).mintNFT(PUG);
+        vm.stopBroadcast();
     }
 }
