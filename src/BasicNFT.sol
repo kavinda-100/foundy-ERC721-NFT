ce5021ec01f9eb4478eb36c4581d4fb39b6fcfc2
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-01 11:57:13 +0530

    feat: implement BasicNFT contract with minting function and token counter

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -4,5 +4,23 @@ pragma solidity ^0.8.25;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-    constructor() ERC721("BasicNFT", "BNFT") {}
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT() public {}
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "https://example.com/api/token/",
+                    Strings.toString(tokenId)
+                )
+            );
+    }
 }
