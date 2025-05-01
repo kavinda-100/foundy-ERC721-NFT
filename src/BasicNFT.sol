83658a8d5327f09d1070643a2cf1524343d4781d
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-01 12:31:43 +0530

    feat: implement mintNFT function and update tokenURI to use stored URIs

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -5,22 +5,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) s_tokenIdToURIs;
 
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNFT() public {}
+    function mintNFT(string memory tokenUri) public {
+        s_tokenIdToURIs[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    "https://example.com/api/token/",
-                    Strings.toString(tokenId)
-                )
-            );
+        return s_tokenIdToURIs[tokenId];
     }
 }
