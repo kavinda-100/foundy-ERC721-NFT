e8780288ee7740acdd7d8b137724593d2d8113cf
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-03 10:49:37 +0530

    feat: enhance MoodNFT contract with mood mapping and tokenURI functionality

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721 {
     uint256 private s_tokenCounter;
     string private s_sadSVGImageURI;
     string private s_happySVGImageURI;
+
+    enum Mood {
+        SAD,
+        HAPPY
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
 
     constructor(
         string memory _sadSVGImageURI,
@@ -15,6 +23,7 @@ contract MoodNFT is ERC721 {
         s_tokenCounter = 0;
         s_sadSVGImageURI = _sadSVGImageURI;
         s_happySVGImageURI = _happySVGImageURI;
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
     }
 
     function mintNFT() public {
@@ -22,7 +31,37 @@ contract MoodNFT is ERC721 {
         s_tokenCounter++;
     }
 
+    function _baseURI() internal view override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
     function tokenURI(
         uint256 tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        string memory imageURI;
+
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            imageURI = s_sadSVGImageURI;
+        } else {
+            imageURI = s_happySVGImageURI;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "',
+                                name(),
+                                '", "description": "Mood NFT", "attributes": [{"trait_type": "mood", "value": "100"}], "image": "',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
 }
